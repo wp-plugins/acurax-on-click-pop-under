1fb0a4d289dbcd6fb0da2d3c2fd5a783663c2377
Acurax <Acurax@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-07 18:19:45 +0000

    Updated readme.txt with proper plugin url
    git-svn-id: https://plugins.svn.wordpress.org/acurax-on-click-pop-under/tags/1.0.1@515992 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ acx_onclick_popunder.php
@@ -0,0 +1,49 @@
+<?php
+/* 
+Plugin Name: Acurax On Click Pop Under
+Plugin URI: http://www.acurax.com/Products/acurax-click-pop-plugin-wordpress/
+Description: An Easy to use plugin to enable pop under page on visitors browser on visitor click. Admin can configure the url to open as popunder on visitor 	
+click. Plugin will set cookie on visitors browser on popunder appear and so it will show only once. You can also configure the cookie timeout in plugin settings.
+Author: Acurax 
+Version: 1.0.1 
+Author URI: http://www.acurax.com 
+License: GPLv2 or later
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/ 
+?>
+<?php
+//*************** Include JS in Header ********
+function enqueue_acx_popunder_script()
+{
+	wp_enqueue_script ( 'acx_pop-under_script', plugins_url('js.php', __FILE__) );
+}
+add_action( 'get_footer', 'enqueue_acx_popunder_script' );
+//*************** Include JS in Header Ends Here ********
+//*************** Admin function ***************
+function acx_onclick_popunder_admin() {
+	include('acx_onclick_popunder_admin.php');
+}
+function acx_onclick_popunder_admin_actions()
+{
+	add_menu_page(  'PopUnder', 'PopUnder', 8, 'Acurax-onclick_popunder-Settings','acx_onclick_popunder_admin',plugin_dir_url( __FILE__ ).'/images/admin.png', 12 ); // 8 for admin
+}
+if ( is_admin() )
+{
+add_action('admin_menu', 'acx_onclick_popunder_admin_actions');
+}
+?>
